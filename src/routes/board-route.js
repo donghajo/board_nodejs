319e6096ee76b86db2f7e8299c9d199dbdd500b1
jodongha <66076070+jodongha@users.noreply.github.com>
2021-08-28 21:47:24 +0900

    no message

--- src/routes/board-route.js
+++ src/routes/board-route.js
@@ -2,7 +2,7 @@ var router = require('express').Router()
 const boardController = require('../controllers/board-controller');
 
 
-router.get('/read/:board_uid', boardController.board_read);
+router.get('/:board_uid', boardController.board_read);
 router.get('/insert', boardController.board_read_insert);
 router.post('/', boardController.board_insert);
 router.patch('/update/:board_uid', boardController.board_update);
