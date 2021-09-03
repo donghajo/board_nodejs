eb6215d90c785eeb1e6c62faa292aa7f6f3f5f6a
jodongha <66076070+jodongha@users.noreply.github.com>
2021-09-03 18:52:25 +0900

    + update

--- src/routes/board-route.js
+++ src/routes/board-route.js
@@ -5,9 +5,10 @@ const pool = require('../database/pool');
 router.get('/list', boardController.board_read);
 router.get('/list/:board_uid', boardController.board_read_content);
 router.get('/insert', boardController.board_read_insert);
+router.get('/list/update', boardController.board_read_update);
 router.post('/insert', boardController.board_insert);
-router.post('/list/update/:board_uid', boardController.board_update);
-router.post('/list/delete', boardController.board_delete);
+router.post('/list/update', boardController.board_update);
+router.post('/list/:board_uid', boardController.board_delete);
 
 
 module.exports = router
