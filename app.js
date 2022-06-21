632a6574632ef3f98b629f9e4af7330b30af8143
jodongha <jodongrami@gmail.com>
2022-06-21 14:29:21 +0900

    method-override

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var methodOverride = require('method-override');
 
 
 var boardRouter = require('./api/v1/src/routes/boardRouter');
@@ -18,6 +19,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(methodOverride("_method", {
+  method: ["POST", "GET"]
+}));
 
 app.use('/', boardRouter);
 
